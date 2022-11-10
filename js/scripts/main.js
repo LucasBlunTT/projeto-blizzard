4ac92b74a0dd9437e59679f512a5f979923a33bf
Lucas da Silva <sklucassilva@gmail.com>
2022-11-10 13:44:53 -0300

    Amarração realizada dos filtros com os games

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -17,10 +17,21 @@ var slide_hero = new Swiper(".slide-principal", {
 });
 
 const allFilters = document.querySelectorAll('.js-nav-games li a');
+const tabPane = document.querySelectorAll('.tab-pane-games');
 
-allFilters.forEach(filter => {
+allFilters.forEach((filter, index) => {
     filter.addEventListener('click', (event) => {
         event.preventDefault();
-        console.log('click');
+
+        allFilters.forEach(item => {
+            item.classList.remove('active');
+        })
+
+        tabPane.forEach(tab => {
+            tab.classList.remove('active');
+        })
+
+        tabPane[index].classList.add('active');
+        filter.classList.add('active');
     })
 })
