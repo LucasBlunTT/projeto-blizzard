740ecb941d94dc14c92a317d84da1866305ac590
Lucas da Silva <sklucassilva@gmail.com>
2022-11-23 20:33:43 -0300

    Modal completo com javascript para fechamento auto

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -49,3 +49,30 @@ btnCloseModal.addEventListener('click', () => {
     let html = document.documentElement;
     html.classList.remove('show-modal')
 })
+
+const btnMenu = document.querySelectorAll('.js-btn-menu');
+const menuSite = document.querySelectorAll('.js-menu');
+
+btnMenu.forEach((btn, index) => {
+    btn.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        menuSite.forEach(itemMenu => {
+            itemMenu.classList.remove('active');
+
+            itemMenu.addEventListener('mouseleave', () => {
+                itemMenu.classList.remove('active');
+                btnMenu.forEach(itemBtn => {
+                    itemBtn.classList.remove('active');
+                })
+            })
+        })
+
+        btnMenu.forEach(itemBtn => {
+            itemBtn.classList.remove('active');
+        })
+
+        btn.classList.add('active');
+        menuSite[index].classList.add('active');
+    })
+})
